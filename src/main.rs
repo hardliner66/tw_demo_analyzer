4c7bbfc74c4e8678a4ec278e19214b6f741cc773
Steve Biedermann <hardliner66@gmail.com>
2024-08-23 23:08:53 +0200

    initial visualization support

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use std::{collections::HashMap, fs::File, io::BufReader, path::PathBuf, process::exit};
 
 use clap::{Parser, Subcommand, ValueEnum};
+use eframe::egui::{self, ComboBox, Key};
+use egui_dropdown::DropDownBox;
+use egui_plot::{Bar, BarChart, GridMark, Line, Plot, PlotPoints};
 use serde::Serialize;
 use stringlit::s;
 use twsnap::{compat::ddnet::DemoReader, enums::HookState, Snap};
+use winit::platform::x11::EventLoopBuilderExtX11;
 
 mod data;
 
@@ -28,7 +34,7 @@ enum ExtractionOutputFormat {
 
 #[derive(Parser, Clone)]
 struct FilterOptions {
-    #[arg(long, default_value = "")]
+    #[arg(short, long, default_value = "")]
     filter: String,
 
     #[arg(short, long)]
@@ -52,7 +58,7 @@ enum Command {
     Analyze {
         #[command(flatten)]
         filter_options: FilterOptions,
-        #[arg(short, long, default_value = "plain")]
+        #[arg(long, default_value = "plain")]
         format: AnalysisOutputFormat,
         path: PathBuf,
     },
@@ -64,8 +70,12 @@ enum Command {
         format: ExtractionOutputFormat,
         path: PathBuf,
     },
+
     #[command(visible_aliases = ["m", "em"])]
     ExtractMap { path: PathBuf },
+
+    #[command(visible_alias = "v")]
+    Visualize { path: PathBuf },
 }
 
 #[derive(Debug, Clone, Default)]
@@ -150,6 +160,166 @@ fn hook_pressed(hs: HookState) -> bool {
         HookState::Grabbed => true,
     }
 }
+
+fn extract(path: PathBuf, filter: &str) -> anyhow::Result<HashMap<String, Vec<Inputs>>> {
+    let file = BufReader::new(File::open(path).unwrap());
+    let mut reader = DemoReader::new(file).expect("Couldn't open demo reader");
+    let mut inputs = HashMap::new();
+    let mut snap = Snap::default();
+    while let Ok(Some(_chunk)) = reader.next_chunk(&mut snap) {
+        for (_id, p) in snap.players.iter() {
+            let name = p.name.to_string();
+            if !name.to_lowercase().contains(&filter.to_lowercase()) {
+                continue;
+            }
+            if let Some(tee) = &p.tee {
+                inputs
+                    .entry(name.clone())
+                    .or_insert_with(|| Vec::<Inputs>::new())
+                    .push(tee.into());
+            }
+        }
+    }
+    Ok(inputs)
+}
+
+#[derive(Default)]
+struct MyApp {
+    names: Vec<String>,
+    inputs: HashMap<String, Vec<Inputs>>,
+    filter: String,
+    selected: SelectedFilter,
+}
+
+#[derive(PartialEq, Eq, Default)]
+enum SelectedFilter {
+    #[default]
+    ShowBoth,
+    ShowHooks,
+    ShowDirections,
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        if ctx.input(|i| i.key_down(Key::Escape)) {
+            exit(0);
+        }
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("My egui Application");
+            let mut reset = false;
+            ui.vertical(|ui| {
+                ui.add(DropDownBox::from_iter(
+                    &self.names,
+                    "test_dropbox",
+                    &mut self.filter,
+                    |ui, text| ui.selectable_label(false, text),
+                ));
+                ComboBox::from_label("filter")
+                    .selected_text(format!(
+                        "{}",
+                        match self.selected {
+                            SelectedFilter::ShowBoth => "Both",
+                            SelectedFilter::ShowHooks => "Hooks",
+                            SelectedFilter::ShowDirections => "Directions",
+                        }
+                    ))
+                    .show_ui(ui, |ui| {
+                        ui.selectable_value(&mut self.selected, SelectedFilter::ShowHooks, "Hooks");
+                        ui.selectable_value(
+                            &mut self.selected,
+                            SelectedFilter::ShowDirections,
+                            "Directions",
+                        );
+                        ui.selectable_value(&mut self.selected, SelectedFilter::ShowBoth, "Both");
+                    });
+                reset = ui.button("Reset").clicked();
+            });
+
+            if let Some(data) = self.inputs.get(&self.filter) {
+                let direction_data: PlotPoints = data
+                    .iter()
+                    .map(|t| {
+                        [
+                            t.tick as f64,
+                            match t.direction {
+                                data::Direction::Left => -1,
+                                data::Direction::None => 0,
+                                data::Direction::Right => 1,
+                            } as f64,
+                        ]
+                    })
+                    .collect();
+
+                let hook_data: Vec<Bar> = data
+                    .iter()
+                    .map(|t| {
+                        let hook = match t.hook_state {
+                            data::HookState::Retracted => 0.0,
+                            data::HookState::Idle => 0.0,
+                            data::HookState::RetractStart => 0.0,
+                            data::HookState::Retracting => 0.0,
+                            data::HookState::RetractEnd => 0.0,
+                            data::HookState::Flying => 0.5,
+                            data::HookState::Grabbed => 0.5,
+                        };
+                        Bar::new(t.tick as f64, hook)
+                    })
+                    .collect();
+
+                let directions = Line::new(direction_data);
+                let hooks = BarChart::new(hook_data);
+                let plot = Plot::new("direction_plot")
+                    .allow_scroll(false)
+                    .y_axis_formatter(|gm, _rng| {
+                        if gm.value < 0.0 {
+                            s!("Left")
+                        } else if gm.value > 0.0 {
+                            if gm.value > 0.4 && gm.value < 0.6 {
+                                s!("Hook")
+                            } else {
+                                s!("Right")
+                            }
+                        } else {
+                            s!("Idle")
+                        }
+                    })
+                    .y_grid_spacer(|_| {
+                        vec![
+                            GridMark {
+                                value: -1.0,
+                                step_size: 1.0,
+                            },
+                            GridMark {
+                                value: 0.0,
+                                step_size: 1.0,
+                            },
+                            GridMark {
+                                value: 0.5,
+                                step_size: 0.5,
+                            },
+                            GridMark {
+                                value: 1.0,
+                                step_size: 1.0,
+                            },
+                        ]
+                    })
+                    .x_axis_formatter(|gm, _rng| format!("{}s", (gm.value / 50.0) as usize));
+                let plot = if reset { plot.reset() } else { plot };
+                plot.show(ui, |plot_ui| match self.selected {
+                    SelectedFilter::ShowBoth => {
+                        plot_ui.line(directions);
+                        plot_ui.bar_chart(hooks)
+                    }
+                    SelectedFilter::ShowHooks => {
+                        plot_ui.line(directions);
+                    }
+                    SelectedFilter::ShowDirections => plot_ui.bar_chart(hooks),
+                });
+            }
+        });
+    }
+}
+
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
@@ -337,28 +507,7 @@ fn main() -> anyhow::Result<()> {
             format,
             filter_options,
         } => {
-            let file = BufReader::new(File::open(path).unwrap());
-            let mut reader = DemoReader::new(file).expect("Couldn't open demo reader");
-            let mut inputs = HashMap::new();
-            let mut snap = Snap::default();
-            while let Ok(Some(_chunk)) = reader.next_chunk(&mut snap) {
-                for (_id, p) in snap.players.iter() {
-                    let name = p.name.to_string();
-                    if !name
-                        .to_lowercase()
-                        .contains(&filter_options.filter.to_lowercase())
-                    {
-                        continue;
-                    }
-                    if let Some(tee) = &p.tee {
-                        inputs
-                            .entry(name.clone())
-                            .or_insert_with(|| Vec::<Inputs>::new())
-                            .push(tee.into());
-                    }
-                }
-            }
-
+            let inputs = extract(path, &filter_options.filter)?;
             let output = match format {
                 ExtractionOutputFormat::Json => {
                     if filter_options.pretty {
@@ -383,6 +532,7 @@ fn main() -> anyhow::Result<()> {
                     }
                 }
             };
+
             if let Some(out) = args.out {
                 std::fs::write(out, output)?;
             } else {
@@ -409,6 +559,38 @@ fn main() -> anyhow::Result<()> {
                 eprintln!("Map not found in demo!");
                 exit(1);
             }
+        }
+        Command::Visualize { path } => {
+            let inputs = extract(path, "")?;
+
+            let options = eframe::NativeOptions {
+                viewport: egui::ViewportBuilder::default(),
+                event_loop_builder: Some(Box::new(|b| {
+                    b.with_x11();
+                })),
+                ..Default::default()
+            };
+            let max_name = inputs
+                .iter()
+                .max_by_key(|i| i.1.len())
+                .unwrap()
+                .0
+                .to_owned();
+            let mut names: Vec<_> = inputs.keys().cloned().collect();
+            names.sort();
+            eframe::run_native(
+                "My egui App",
+                options,
+                Box::new(|_| {
+                    Ok(Box::<MyApp>::new(MyApp {
+                        names,
+                        inputs,
+                        filter: max_name,
+                        ..Default::default()
+                    }))
+                }),
+            )
+            .unwrap();
         }
     }
 
